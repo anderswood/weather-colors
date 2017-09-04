158bb9613a9e6231927763d81467f9368a0d6566
Anders Wood <anderswood@gmail.com>
2017-09-03 19:00:33 -0600

    fix color object syntax

--- client/src/conditionsObj.js
+++ client/src/conditionsObj.js
@@ -13,6 +13,9 @@ export const conditions = {
   wind: '106, 142, 163', //#6a8ea3
   fog: '177, 215, 215', //#B1D7D7
   cloudy: '237, 230, 227', //#ede6e3
-  'partly-cloudy-day': '247, 178, 103', // #F7B267
+  'partly-cloudy-day': {
+    color: '247, 178, 103', // #F7B267
+    backgroundURL: ''
+  },
   'partly-cloudy-night': '142, 102, 58' // #8e663a
 }
